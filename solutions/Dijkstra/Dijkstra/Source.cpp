704d21392f171bc027aee841317fa5e6a07d25c6
Vladimir <vovakalinin1997@gmail.com>
2016-05-06 02:31:22 +0300

    test1

---
+++ solutions/Dijkstra/Dijkstra/Source.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "Graph.h"
+
+int main() {
+  Graph g(5);
+  g.AddEdge(1, 2, 3);
+ // printf("%i\n", g.IsConnect(1, 2));
+//  printf("%i\n", g.IsConnect(2, 1));
+//  printf("%i\n", g.IsConnect(3, 1));
+//  printf("%i\n", g.IsConnect(1, 3));
+//  printf("%i\n", g.IsConnect(4, 5));
+}
